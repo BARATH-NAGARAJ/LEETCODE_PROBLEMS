7fd7658656e61d2e6ea54979a7fbe9e1f5fe513f
BARATH-NAGARAJ <barathnagaraj12@gmail.com>
2024-09-30 07:05:02 +0530

    Time: 2079 ms (6.89%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select product_name,year,price from Sales join Product on Product.product_id=Sales.product_id;
+select product_name,year,price from Sales join Product on Sales.product_id=Product.product_id;
